bf205eb7ebac087a28a45a95108023de630732cf
Lawrence Manning <lawrence@aslak.net>
2019-10-02 21:55:12 +0100

    Extended exception handling
    Now reports at least address error, illegal instruction, div by 0.

--- exceptions.s
+++ exceptions.s
@@ -1,38 +1,64 @@
 		.include "include/hardware.i"
+		.include "include/vectors.i"
 
 		.global exceptionsinit
 
 		.section .vectors
 		.align 2
 
-evinitialsp:	.space 4
-evinitialpc:	.space 4
-evaccessfault:	.space 4
-evaddresserr:	.space 4
-evillegalinst:	.space 4
+vectortable:	.space 256*4
 
 		.section .text
 		.align 2
 
-exceptionsinit:	move.l #accessfault,evaccessfault
-		move.l #addresserr,evaddresserr
-		move.l #illegalinst,evillegalinst
+exceptionsinit:	move.l #buserror,VBUSERROR
+		move.l #addresserr,VADDRESSERROR
+		move.l #illegalinst,VILLEGALINSTRUCTION
+		move.l #zerodivide,VZERODIVIDE
 		rts
 
-accessfault:	lea (accessfaultmsg,%pc),%a0
+buserror:	lea (buserrormsg,%pc),%a0
+		lea (8,%sp),%sp			| advance to sr/pc
 		bra 2f
 addresserr:	lea (addresserrmsg,%pc),%a0
+		lea (8,%sp),%sp			| advance to sr/pc
 		bra 2f
 illegalinst:	lea (illegalinstmsg,%pc),%a0
 		bra 2f
+zerodivide:	lea (zerodividemsg,%pc),%a0
+		bra 2f
 
 2:		move.b #0xff,LED
+		bsr putstr
+		movea.l #buffer,%a0
+		lea (srmsg,%pc),%a1
+		bsr strconcat
+		move.w 0(%sp),%d0
+		bsr wordtoascii
+		lea (spacesmsg,%pc),%a1
+		bsr strconcat
+		lea (pcmsg,%pc),%a1
+		bsr strconcat
+		move.l 2(%sp),%d0
+		bsr longtoascii
+		lea (newlinemsg,%pc),%a1
+		bsr strconcat
+		movea.l #buffer,%a0
 		bsr putstr
 9:		bra 9b
 
 		.section .rodata
 		.align 2
 
-accessfaultmsg:	.asciz "\r\n*** Access fault exception, halt.\r\n"
-addresserrmsg:	.asciz "\r\n*** Address error exception, halt.\r\n"
+buserrormsg:	.asciz "\r\n*** Bus error, halt.\r\n"
+addresserrmsg:	.asciz "\r\n*** Address error, halt.\r\n"
 illegalinstmsg:	.asciz "\r\n*** Illegal instruction, halt.\r\n"
+zerodividemsg:	.asciz "\r\n*** Divide by zero, halt.\r\n"
+srmsg:		.asciz "SR: "
+pcmsg:		.asciz "PC: "
+spmsg:		.asciz "SP: "
+
+		.section .bss
+		.align 2
+
+buffer:		.space 256
