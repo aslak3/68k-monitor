5530ff00e3b35a5795d4c3161c73faff41df9916
Lawrence Manning <lawrence@aslak.net>
2025-12-06 22:14:08 +0000

    Added the bit instructions to the disassembler
    This is just more instruction defs pretty much.

--- os-test/disasstest.s
+++ os-test/disasstest.s
@@ -12,6 +12,13 @@ disasstest:	ori.b #0x12,%ccr
 		addi.b #0x7f,(0x12345678)
 		eori.l #0xfeedface,(0x66666666)
 		eori.b #0x2a,%ccr
+		btst.b #31,(%a0)
+		btst #31,%d1
+		btst %d0,%d1
+		bchg.b #3,(0x7f,%d2.l,%a1)
+		bchg %d2,%d3
+		bclr.b #5,(%d2.w,%a2)
+		bclr %d4,%d5
+		bset.b #7,(0x100,%a3)
+		bset %d6,%d7
 
-		nop
-		rts
